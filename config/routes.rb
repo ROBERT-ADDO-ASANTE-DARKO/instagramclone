7a4a91c1bb00bf65b6284214f38e5b78b0914bf6
Robert Yaw Agyekum Addo <300621923815@bluecrest.edu.gh>
2024-01-16 14:48:22 -0800

    Instagram app updated 	new file:   app/controllers/users_controller.rb 	modified:   app/views/layouts/application.html.erb 	new file:   app/views/users/_post.html.erb 	new file:   app/views/users/show.html.erb 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  # get 'pages/home'
+  root "pages#home"
   devise_for :users,
     path: '',
     path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
@@ -13,7 +14,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :users, only: [:show]
+  # get 'users/:id', to: 'users#show'
+  # /users/3 -> Users controller, show action, params {id: '3'}
 
   # Defines the root path route ("/")
-  root "pages#home"
+  # root "pages#home"
+  #resources :users, only: [:show]
 end
